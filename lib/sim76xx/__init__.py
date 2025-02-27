fb352260816b9d8e42f3c5b5684ad6c2f6b386ae
Garatronic <fpierson@garatronic.fr>
2025-02-27 15:02:41 +0100

    Update __init__.py
    Uncomment SMS, Voice and Phonebook

--- lib/sim76xx/__init__.py
+++ lib/sim76xx/__init__.py
@@ -1,9 +1,9 @@
 from .core import SIM76XX, Notifications
 from .sms import SMSError
-#from .sms import SMS
-#from .calling import Calling
+from .sms import SMS
+from .voice import Voice
 #from .gprs import GPRS
 #from .http import HTTP
 #from .ftp import FTP
-#from .phonebook import Phonebook
+from .phonebook import Phonebook
 #from .tcpip import TCPIP
